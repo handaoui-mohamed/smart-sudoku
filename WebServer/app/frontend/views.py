109a1df7d4021e38823cb97fbc0a9226f3f0a2c9
handaoui-mohamed <handaoui.mohamed@gmail.com>
2019-02-09 16:56:42 +0100

    minor changes

--- WebServer/app/frontend/views.py
+++ WebServer/app/frontend/views.py
@@ -1,18 +1,22 @@
 # -*- coding: utf-8 -*-
 from app import app, db
 
+
 @app.route('/')
 @app.route('/home')
 def home():
     return app.send_static_file("index.html")
 
+
 @app.route('/register')
 def register():
     return app.send_static_file("register.html")
 
+
 @app.route('/new-grid')
 def newGrid():
     return app.send_static_file("new_grid.html")
+
 
 @app.errorhandler(500)
 def error(e):
